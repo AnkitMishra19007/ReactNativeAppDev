7d37ce46cc60f1936e8f8688b92ef7022ccd9a34
Utkarsh <56065899+utkarshanand11@users.noreply.github.com>
2020-09-13 13:55:45 +0530

    homepage: with branches & sub-branches

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Navigator from './routes/startStack';
 
-import Navigator from './routes/catStack';
 export default function App() {
   return (
     <Navigator />
