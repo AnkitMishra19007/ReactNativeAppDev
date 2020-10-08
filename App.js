00b54d5898ef7e63d5c515573fcf1e1fee13b107
Utkarsh <56065899+utkarshanand11@users.noreply.github.com>
2020-10-08 13:13:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config';
 import Navigator from './routes/startStack';
 
+firebase.initializeApp(firebaseConfig);
 export default function App() {
   return (
     <Navigator />
