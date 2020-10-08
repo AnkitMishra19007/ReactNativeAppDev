2a4a91d70247b4b2c1f6e0d18a3ee3b55ada4856
Utkarsh <56065899+utkarshanand11@users.noreply.github.com>
2020-10-08 13:17:56 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQuY6csK6-vF4BQjjXK_SfcmUQZ7dCn6s",
+    authDomain: "kiranaa-e75c2.firebaseapp.com",
+    databaseURL: "https://kiranaa-e75c2.firebaseio.com",
+    projectId: "kiranaa-e75c2",
+    storageBucket: "kiranaa-e75c2.appspot.com",
+    messagingSenderId: "18584016609",
+    appId: "1:18584016609:web:c4d982ee6156e4472709a0",
+    measurementId: "G-ST7NLFHDYJ"
+};
