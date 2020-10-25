da09da96a67c3f28fc67c9a419a0e1b92d9e0f22
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2020-10-25 21:53:40 +0530

    Add files via upload

---
+++ Kiranaa/routes/drawerCustomer.js
@@ -0,0 +1,75 @@
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { Image } from 'react-native';
+import buyerStack from './buyerStack';
+import CustomerProfile from './CustomerProfile';
+import CustomerPast from './CustomerPast';
+import contactUsSeller from './contactUsSeller';
+import { color } from 'react-native-reanimated';
+import customerPast from '../screens/customerPast';
+
+const RootDrawerNavigator=createDrawerNavigator({
+    Home:{
+        screen: buyerStack,//What we want to show
+        navigationOptions:{
+            title: 'Products',
+            drawerIcon: (
+                <Image
+                style={{width:24,height:24}}
+                source={require("../assets/icons/box.png")}
+                />
+            ),
+        }
+
+    },
+    MyProfile: {
+        screen: CustomerProfile,
+        navigationOptions:{
+            title: 'My Profile',
+            drawerIcon: (
+                <Image
+                    style={{ width: 24, height: 24 }}
+                    source={require("../assets/icons/user.png")}
+                />
+            ),
+        }
+    },
+    History: {
+        screen: CustomerPast,
+        navigationOptions:{
+            title: 'History',
+            drawerIcon: (
+                <Image
+                style={{width:24,height:24}}
+                source={require("../assets/icons/history.png")}
+                />
+            ),
+        }
+    },
+    Complain: {
+        screen: CustomerProfile,
+        navigationOptions:{
+            title: 'Complain',
+            drawerIcon: (
+                <Image
+                style={{width:24,height:24}}
+                source={require("../assets/icons/inbox.png")}
+                />
+            ),
+        }
+    },
+    LogOut: {
+        screen: CustomerProfile,
+        navigationOptions:{
+            title: 'Log Out',
+            drawerIcon: (
+                <Image
+                style={{width:24,height:24}}
+                source={require("../assets/icons/out.png")}
+                />
+            ),
+        }
+    },
+});
+export default createAppContainer(RootDrawerNavigator);
