0b666025ca9a017df3c81b8ccc717b9684098e13
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2020-11-04 22:44:10 +0530

    Added Fonts configuration

--- Kiranaa/App.js
+++ Kiranaa/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 // import Navigator from './routes/startStack';
 import * as firebase from 'firebase';
 import { firebaseConfig } from './config';
 import Navigator from './routes/startStack';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFont= ()=>{
+  return Font.loadAsync({
+    'Patua': require('./assets/fonts/PatuaOne-Regular.ttf'),
+    'Marker': require('./assets/fonts/PermanentMarker-Regular.ttf')
+  });
+};
 
 firebase.initializeApp(firebaseConfig);
 export default function App() {
+  const [fontLoaded, setfontLoaded] = useState(false);
+
+ if(!fontLoaded){
+   return <AppLoading
+   startAsync={fetchFont}
+    onError={()=> console.log("Error")}
+    onFinish={()=>{
+    setfontLoaded(true);
+   }}
+   />
+ };
+
   return (
     <Navigator />
   );
