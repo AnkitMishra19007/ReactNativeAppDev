6e7abb26ea41f365d3b4ff3136580a0a5a2820b7
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2020-11-09 10:24:45 +0530

    Update BuyerHome.js

--- Kiranaa/screens/BuyerHome.js
+++ Kiranaa/screens/BuyerHome.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Button, Image, Text, View, Alert, TouchableOpacity } from 'react-native';
 
-
+//<TouchableOpacity onPress={() => navigation.navigate('StationaryShopsCategory')}>
+//<Image source={require('../assets/stationary.png')} /></TouchableOpacity>
 
 export default function BuyerHome({ navigation }) {
 
@@ -9,35 +10,48 @@ export default function BuyerHome({ navigation }) {
     return (
 
         <View style={styles.container}>
-            <Text style={styles.txt2}>--Choose any shop from below--</Text>
-            <View style={styles.upper}>
-                <View style={styles.left}>
-                    <TouchableOpacity onPress={() => navigation.navigate('StationaryShopsCategory')}>
-                        <Image source={require('../assets/stationary.png')} /></TouchableOpacity>
-                    <Text style={styles.txt}>Stationary</Text>
+            <Text style={styles.txt1}>Available shops near you</Text>
 
-                </View>
-                <View style={styles.right}>
-                    <TouchableOpacity onPress={() => navigation.navigate('GeneralShopsCategory')}>
-                        <Image source={require('../assets/supermarket.png')} /></TouchableOpacity>
-                    <Text style={styles.txt}>General</Text>
-
-                </View>
+            
+            <View style={styles.top}>
+            <TouchableOpacity onPress={() => navigation.navigate('StationaryShopsCategory')} style={styles.part}>
+                <Image source={require('../assets/stationary.png')} style={{height: 40, width: 40}}/>
+                <Text style={styles.txt}>Stationary</Text>
+            </TouchableOpacity>
+            <View style={styles.down}>
+                <Text style={styles.txt2}>4</Text>
             </View>
-            <View style={styles.lower}>
-                <View style={styles.left}>
-                    <TouchableOpacity onPress={() => navigation.navigate('PlasticsShopsCategory')}>
-                        <Image source={require('../assets/sand.png')} /></TouchableOpacity>
-                    <Text style={styles.txt}>Plastics</Text>
-
-                </View>
-                <View style={styles.right}>
-                    <TouchableOpacity onPress={() => navigation.navigate('GroceriesShopsCategory')}>
-                        <Image source={require('../assets/vegetable.png')} /></TouchableOpacity>
-                    <Text style={styles.txt}>Groceries</Text>
-                </View>
             </View>
 
+            <View style={styles.top}>
+            <TouchableOpacity onPress={() => navigation.navigate('GeneralShopsCategory')} style={styles.part}>
+                <Image source={require('../assets/supermarket.png')} style={{height: 40, width: 40}}/>
+                <Text style={styles.txt}>General Store</Text>
+            </TouchableOpacity>
+            <View style={styles.down}>
+                <Text style={styles.txt2}>0</Text>
+            </View>
+            </View>
+
+            <View style={styles.top}>
+            <TouchableOpacity onPress={() => navigation.navigate('PlasticsShopsCategory')} style={styles.part}>
+                <Image source={require('../assets/sand.png')} style={{height: 40, width: 40}}/>
+                <Text style={styles.txt}>Plasticware</Text>
+            </TouchableOpacity>
+            <View style={styles.down}>
+                <Text style={styles.txt2}>1</Text>
+            </View>
+            </View>
+
+            <View style={styles.top}>
+            <TouchableOpacity onPress={() => navigation.navigate('GroceriesShopsCategory')} style={styles.part}>
+                <Image source={require('../assets/vegetable.png')} style={{height: 40, width: 40}}/>
+                <Text style={styles.txt}>Groceries</Text>
+            </TouchableOpacity>
+            <View style={styles.down}>
+                <Text style={styles.txt2}>2</Text>
+            </View>
+            </View>
         </View>
 
     );
@@ -49,53 +63,47 @@ const styles = StyleSheet.create({
         backgroundColor: "#ddd",
         flexDirection: 'column',
     },
-    txt: {
-        color: '#123',
-        fontSize: 17,
-        fontFamily: 'Patua',
-        padding: 4,
-        alignSelf: 'center',
-    },
-    txt2: {
-        color: '#123',
-        fontSize: 20,
-        fontFamily: 'Patua',
-        padding: 4,
-        alignSelf: 'center',
-        backgroundColor: '#ddd',
-    },
-    upper: {
-        flex: 1,
-        backgroundColor: '#ddd',
+    top:{
         flexDirection: 'row',
-    },
-    lower: {
         flex: 1,
-        backgroundColor: '#ddd',
+        backgroundColor: '#E3CBAE',
+        padding: 10,
+        marginTop: 25,
+        marginBottom: 25,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 20,
+    },
+    down:{
+        flex: 0.2,
+        alignItems: 'center',
+        backgroundColor: 'indianred',
+        justifyContent: 'center',
+        margin: 9,
+        borderRadius: 20,
+    },
+    txt1:{
+        fontFamily: 'Patua',
+        fontSize: 22,
+        alignSelf: 'center',
+        marginTop: 5,
+    },
+    txt2:{
+        fontFamily: 'Patua',
+        fontSize: 22,
+        color: 'white'
+    },
+    txt:{
+        marginLeft: '5%',
+        fontFamily: 'Patua',
+        fontSize: 22,
+        alignSelf: 'center'
+    },
+    part:{
         flexDirection: 'row',
-    },
-    left: {
-        flex: 1,
-        backgroundColor: '#dcb',
-        justifyContent: 'center',
         alignItems: 'center',
-        borderWidth: 2,
-        borderColor: 'indianred',
-        borderRadius: 30,
-        margin: 14,
-    },
-    right: {
+        padding: 10,
         flex: 1,
-        backgroundColor: '#dcb',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 2,
-        borderColor: 'indianred',
-        borderRadius: 30,
-        margin: 14,
-    },
-    img: {
-        borderWidth: 2,
     }
 
 });
